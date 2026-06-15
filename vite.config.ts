3e469d6841af0b2e73b9fa97f64bc064c5f5e8f7
wajihacodeofficial <wajihacodeofficial@gmail.com>
2026-06-15 07:46:41 +0500

    chore: set base path to relative and update local database file

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import renderer from "vite-plugin-electron-renderer";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "./",
   server: {
     host: "::",
     port: 8080,
